70dfcb87f537c8f8810a68d736a9b754627d6f09
Muciej <kejdzuuu@gmail.com>
2020-12-30 21:42:55 +0100

    Ex. 4.2

---
+++ blog_list/app.js
@@ -0,0 +1,15 @@
+const config = require('./utils/config')
+const http = require('http')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const blogsRouter = require('./controllers/blogs')
+
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+
+app.use(cors())
+app.use(express.json())
+app.use('/', blogsRouter)
+
+module.exports = app
