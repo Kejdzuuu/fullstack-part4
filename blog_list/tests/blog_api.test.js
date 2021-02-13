9f034f8f5d39f224004449f0f1b60b7092edcf3a
Muciej <kejdzuuu@gmail.com>
2021-02-13 13:10:21 +0100

    Ex.4.10

--- blog_list/tests/blog_api.test.js
+++ blog_list/tests/blog_api.test.js
@@ -6,6 +6,11 @@ const testData = require('./test_blog_data').listWithManyBlogs
 
 const api = supertest(app)
 
+const blogsInDb = async () => {
+  const blogs = await Blog.find({})
+  return blogs.map(blog => blog.toJSON())
+}
+
 beforeEach(async () => {
   await Blog.deleteMany({})
 
@@ -22,6 +27,23 @@ test('correct amount of blogs returned', async () => {
 test('blog post has an id property', async () => {
   const response = await api.get('/api/blogs')
   expect(response.body[0].id).toBeDefined()
+})
+
+test('a blog post can be added', async () => {
+  const newBlog = {
+    title: 'Test Blog',
+    author: 'Author',
+    url: 'http://www.blog.pl',
+    likes: 5,
+  }
+
+  await api.post('/api/blogs').send(newBlog).expect(201).expect('Content-Type', /application\/json/)
+
+  const blogs = await blogsInDb()
+  expect(blogs).toHaveLength(testData.length + 1)
+  expect(blogs[blogs.length-1].title).toBe(newBlog.title)
+  expect(blogs[blogs.length-1].author).toBe(newBlog.author)
+  expect(blogs[blogs.length-1].url).toBe(newBlog.url)
 })
 
 afterAll(() => {
